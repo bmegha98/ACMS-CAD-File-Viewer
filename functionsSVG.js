6932827e9d47d80c27507110ea97ddf659554527
anshula16 <anshulasachdeva16@gmail.com>
2020-05-14 01:05:06 +0530

    Undo functionality added.

--- functionsSVG.js
+++ functionsSVG.js
@@ -1,5 +1,36 @@
-function loadSVG() 
-{
+class Stack{
+
+    constructor(){
+
+        this.stack = [];
+        this.top = -1;
+    }
+
+    isEmpty(){
+
+        return this.top === -1;
+    }
+
+    push(item){
+
+        this.top++;
+        this.stack[this.top] = item;
+    }
+
+    pop(){
+
+        if(!this.isEmpty()){
+            var item = this.stack[this.top];
+            this.top--;
+            return item;
+        }
+    }
+}
+
+stk = new Stack();
+
+function loadSVG() {
+
     var object = document.getElementById('svg_object');
     var svgObject = object.contentDocument;
     
@@ -14,55 +45,74 @@ function loadSVG()
     
     var element = svg.getElementsByTagName('*');
     var i;
-    for(i = 0; i < element.length; i++)
-    {
-        element[i].addEventListener("click", function()
-        {
-            if(svg.getElementById(this.id) != null)
-            {
-                svg.getElementById(this.id).style.stroke = 'red';
-                svg.getElementById(this.id).style["stroke-width"] = "2";
+    for(i = 0; i < element.length; i++){
+
+        element[i].addEventListener("click", function(){
+        
+            if(svg.getElementById(this.id) != null){
+                svg.getElementById(this.id).classList.add("click");
             }
         });
     }
 }
 
+function undo(){
+    
+    while(!stk.isEmpty()){
+        
+        var item = stk.pop();
 
-function applyCSS()
-{
+        if(item.tagName === 'g'){
+
+            var tempParts = item.getElementsByTagName("*");
+            var i;
+            for(i = 0; i < tempParts.length; i++){
+
+                tempParts[i].classList.remove("cluster_rack_shelf");
+                tempParts[i].classList.remove("click");
+            }
+        }
+        else{
+
+            item.classList.remove("bin");
+            item.classList.remove("click");            
+        }
+    }
+}
+
+function applyCSS(){
+
     var square = document.getElementById("svgID").value;
     var svgObject = document.getElementById('svg_object').contentDocument;
     var svg = svgObject.getElementsByTagName('svg')[0];
     var element = svg.getElementById(square);
-
+        
     if(element == null)
         alert("No such element exists!!");
-    else
-      {
-      //for particular square
-      if(element.tagName === 'g')
-      {
-          var parts = element.getElementsByTagName("*");
-          var i;
-          for(i= 0;i < parts.length;i++)
-          {
-              //parts[i].setAttribute('class','change')
-              parts[i].classList.add('change');
-              parts[i].classList.remove('path_change');
-              //parts[i].style.stroke = "salmon";
-              //parts[i].style["stroke-width"] = "2";
-              window.scrollTo(parts[i].getAttribute('x') - 200, parts[i].getAttribute('y') - 200);
-          }
-      }
-      //for particular path
-      else
-      {
-          //element.setAttribute('class','path_change');
-          element.classList.add('path_change');
-          // element.style.stroke = "lightgreen";
-          // element.style["stroke-width"] = "2";
+    else{
+        undo();
 
-          window.scrollTo(element.getAttribute('x') - 200, element.getAttribute('y') - 200);
-      }
-  }
+        stk.push(element);
+        //for particular cluster/rack/shelf
+        if(element.tagName === 'g'){    
+
+            var parts = element.getElementsByTagName("*");
+            var i;
+            for(i = 0; i < parts.length; i++){
+
+                parts[i].classList.add("cluster_rack_shelf");
+                parts[i].classList.remove("bin");
+                
+                window.scrollTo(parts[i].getAttribute('x') - 200, parts[i].getAttribute('y') - 200);
+            }
+        }
+        //for particular bin
+        else{ 
+            
+            element.classList.add("bin");
+            
+            window.scrollTo(element.getAttribute('x') - 200, element.getAttribute('y') - 200);
+        }
+    }
 }
+
